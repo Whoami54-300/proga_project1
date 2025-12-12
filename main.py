646f8a1d79ca9489a13fd901b4c84b5030a7b82f
Maxim Kharchenko <maxim28032020@gmail.com>
2025-12-12 14:32:34 +0600

    ПР 10. таблица умножения

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ print(f'Здравствуйте {x} {y} - вам {z} лет')
 
 # ПР 9
 
+"""
 x = int(input('Введите четырехзначный год: '))
 if x % 4 == 0 and x % 100 != 0:
     print('Год високосный')
@@ -17,3 +18,19 @@ elif x % 400 == 0:
     print('Год високосный')
 else:
     print('Год не високосный(')
+"""
+
+# ПР 10
+
+a = []
+
+for i in range(1, 11):
+    k = []
+    for j in range(1, 11):
+        k.append(i * j)
+    a.append(k)
+
+for k in a:
+    for num in k:
+        print(f"{num:3}", end=" ")
+    print()
