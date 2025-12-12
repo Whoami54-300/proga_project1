c2a7799e8ea0db354a4edb1454988815ef7f9189
Maxim Kharchenko <maxim28032020@gmail.com>
2025-12-12 14:07:16 +0600

    Добавил Inline кнопки

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 
 
 bot = telebot.TeleBot("7861777768:AAFCX0hBOTCZtuDAqRiySmK4UJEbYSHpk3Y")
@@ -7,6 +8,19 @@ bot = telebot.TeleBot("7861777768:AAFCX0hBOTCZtuDAqRiySmK4UJEbYSHpk3Y")
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, f"Привет, {message.from_user.first_name} !")
+    keyboard = types.InlineKeyboardMarkup()
+    btn_yes = types.InlineKeyboardButton(text="Да", callback_data="productive_yes")
+    btn_no = types.InlineKeyboardButton(text="Нет", callback_data="productive_no")
+    keyboard.add(btn_yes, btn_no)
+    bot.send_message(message.chat.id, "Вы продуктивный?!", reply_markup=keyboard)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_handler(call):
+    if call.data == "productive_yes":
+        bot.send_message(call.message.chat.id, "Это ооочень хорошо)")
+    elif call.data == "productive_no":
+        bot.send_message(call.message.chat.id, "ПОШЕЛ ОТ СЮДА!")
+    bot.answer_callback_query(call.id)
 
 
 
