72fe99b9d9226d32b2f122726ff4c61b4b31f15a
Maxim Kharchenko <maxim28032020@gmail.com>
2025-12-12 17:58:42 +0600

    Добавил код с бд в осн код (надо разобраться)

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,5 +1,8 @@
 import telebot
 from telebot import types
+# from aiogram import types, executor
+# import sheduler
+# import dp
 
 
 bot = telebot.TeleBot("7861777768:AAFCX0hBOTCZtuDAqRiySmK4UJEbYSHpk3Y")
@@ -21,7 +24,24 @@ def callback_handler(call):
     elif call.data == "productive_no":
         bot.send_message(call.message.chat.id, "ПОШЕЛ ОТ СЮДА!")
     bot.answer_callback_query(call.id)
+    # bot.send_message(message.chat.id, "Напиши задачу. Если надо будет удалить, то пиши '-'")
 
+# @dp.message_handler(lambda message: message.text.startswith('-'))
+# async def del_task(message: types.Message):
+#     """Удаляет одну задачу по её идентификатору (id)"""
+#     row_id = int(message.text[1:])
+#     sheduler.delete_task(row_id)
+#     answer_message = "Задача успешно удалена."
+#     await message.answer(answer_message)
 
+# @dp.message_handler()
+# async def add_task(message: types.Message):
+#     """Добавляет задачу в планировщик"""
+#     if not message.text.startswith("/"):
+#         chat_id = message.chat.id
+#         await sheduler.add_task(message.text, chat_id)
+
+# if __name__ == "__main__":
+#     executor.start_polling(dp, skip_updates=True)
 
 bot.polling(none_stop=True)
